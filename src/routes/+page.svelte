1c26db93bae57dcf7f99ba4719f319dbd73e04bb
jdmedlock <jdmedlock@gmail.com>
2022-12-12 17:22:49 -0600

    Feature: Add checkout form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
+<script>
+  let quantity = 0
+  let unitPrice = 34.99
+  let totalCost = 0
+
+  const handleCheckout = () => {
+    totalCost = quantity * unitPrice
+  }
+</script>
+
 <section class="ml-6 mr-8">
-  <h1 class="text-3xl font-bold underline">Chingu Roundtable</h1>
+  <div class="flex flex-row h-full w-full justify-center pt-4">
+    <div class="place-self-center pt-4 lg:pt-0">
+      <img class="ml-20 w-16 h-16 invisible md:visible" 
+        src="Chingu.jpg" alt="Chingu Logo"/>
+      <h1 class="pt-4 text-3xl font-bold underline">Chingu Roundtable</h1>
+    </div>
+  </div>
   <h2 class="mt-4 text-2xl font-bold">How to integrate PayPal REST in your apps</h2>
   <p class="mt-4 text-xl">
     This app was built to provide a test platform to show how to integrate PayPal 
@@ -15,15 +31,26 @@
 <!-- Mock Checkout Form -->
 <section class="ml-6 mr-8">
   <h2 class="mt-4 text-2xl font-bold">Mock Checkout Form</h2>
-  <form method="POST">
-    <label>
-      Email
-      <input class="border" name="email" type="email">
-    </label>
-    <label>
-      Password
-      <input class="border" name="password" type="password">
-    </label>
-    <button>Log in</button>
-  </form>
+  <div class="mt-4 ml-8 text-lg">
+    <span class="font-bold">Item:</span>
+    <span>Left handed Monkey wrench</span>
+    <div class="mt-4">
+      <label for="quantity">
+        Quantity:
+        <input class="border w-12" id="quantity" name="quantity" type="number"
+        bind:value={ quantity } on:input={ quantity }>
+      </label>
+      <label for="unitPrice">
+        Unit price:
+        <input class="border w-16" id="unitPrice" name="unitPrice" type="number"
+        bind:value={ unitPrice } disabled>
+      </label>
+      <div class="mt-4">
+        Total cost: { totalCost }
+      </div>
+      <div class="flex flex-col items-center mt-4 w-1/6 bg-green-500 border rounded-lg">
+        <button class="font-bold" on:click={ handleCheckout }>Checkout</button>
+      </div>
+    </div>
+  </div>
 </section>
