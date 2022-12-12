613650dc142db85b1630e2261f185a7e5c33e03b
jdmedlock <jdmedlock@gmail.com>
2022-12-12 10:54:04 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<section class="ml-6 mr-8">
+  <h1 class="text-3xl font-bold underline">Chingu Roundtable</h1>
+  <h2 class="mt-4 text-2xl font-bold">How to integrate PayPal REST in your apps</h2>
+  <p class="mt-4 text-xl">
+    This app was built to provide a test platform to show how to integrate PayPal 
+    payments into your applications. It utilizes the PayPal REST API and 
+    `@paypal/paypal-js` to add payment options to a checkout form.
+
+    This will be referenced in an upcoming weekly Chingu Roundtable in our
+    <a class="underline italic text-blue-500" target="_blank noreferrer" 
+      href="https://www.youtube.com/playlist?list=PLmnPfbM4_HyszkLRkLtlUgoMCHer3jpg7">YouTube channel.</a>
+  </p>
+</section>
+
+<!-- Mock Checkout Form -->
+<section class="ml-6 mr-8">
+  <h2 class="mt-4 text-2xl font-bold">Mock Checkout Form</h2>
+  <form method="POST">
+    <label>
+      Email
+      <input class="border" name="email" type="email">
+    </label>
+    <label>
+      Password
+      <input class="border" name="password" type="password">
+    </label>
+    <button>Log in</button>
+  </form>
+</section>
